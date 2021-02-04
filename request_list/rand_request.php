a53d5242125b221324c0e11d1b47498b32d9bce3
MrTwinkles47 <64985827+MrTwinkles47@users.noreply.github.com>
2021-02-04 14:58:05 -0500

    Update rand_request.php
    Or what?

--- request_list/rand_request.php
+++ request_list/rand_request.php
@@ -84,7 +84,7 @@ if(isset($_GET["broadcaster"]) && !empty($_GET["broadcaster"])){
 //get number of random requests, if not specified, set as 1
 if (isset($_GET["num"]) && !empty($_GET["num"]) && is_numeric($_GET["num"]) && $_GET["num"] > 0){
 	$num = $_GET["num"];
-}elseif(!isset($_GET["num"]) && empty($_GET["num"])){
+}elseif(!isset($_GET["num"]) || empty($_GET["num"])){
 	$num = 1;
 }else{ 
 	die("Good one, $user, but only positive integers are allowed!");
